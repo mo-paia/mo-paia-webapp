7652ffb4a84d3dcfd151ad0b159fa9f57d418bfd
Danilo Perassoli de Souza <danilo.souza@ifood.com.br>
2019-07-06 14:42:00 -0300

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    {/* <div className="logo" /> */}
+    {/* <p>
+      Sample project provided by <a href="https://auth0.com">Auth0</a>
+    </p> */}
+    <p>
+      <small>
+        Powered by Mobalho
+      </small>
+    </p>
+  </footer>
+);
+
+export default Footer;
